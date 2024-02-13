1f99e4ca04447fa71a6848814b64526e93cafadf
BeccaK8 <150313386+BeccaK8@users.noreply.github.com>
2024-02-13 11:02:52 -0600

    setup buttons for date navigation

--- src/components/healthDates/HealthDateNavigation.js
+++ src/components/healthDates/HealthDateNavigation.js
@@ -1,28 +1,45 @@
+import { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import get_formatted_health_date from '../../lib/get_formatted_health_date'
+
 const HealthDateNavigation = (props) => {
 
     const { msgAlert, user } = props
-    const showDateObj = new Date(props.showDate)
-    console.log('showDateObj = ', showDateObj)
 
-    const startOfWeek = () => {
-        console.log('showDateObj = ', showDateObj)
-        const diff = showDateObj.getDate() - showDateObj.getDay() + (showDateObj.getDay() === 0 ? 6 : 1)
-        return new Date(showDateObj.setDate(diff))
+    const [ showDate, setShowDate ] = useState(props.showDate)
+    const showDateObj = new Date(showDate)
+    console.log('showDateObj = ', showDateObj)
+    
+    const diff = showDateObj.getDate() - showDateObj.getDay() + (showDateObj.getDay() === 0 ? 6 : 1)
+    const startOfWeek = new Date(showDateObj.setDate(diff))
+
+    let dateDivs = []
+    dateDivs.push(<div><FontAwesomeIcon icon={ faChevronLeft }/></div>)
+    for (let i = 0; i < 7; i++) {
+        const thisCardDate = new Date(startOfWeek)
+        thisCardDate.setDate(startOfWeek.getDate() + i)
+        const formattedHealthDate = get_formatted_health_date(thisCardDate)
+        const showing = (formattedHealthDate === showDate)
+        // console.log(`showing = ${showing}, formattedHealthDate = ${formattedHealthDate}, showDate=${showDate}`)
+        dateDivs.push(
+            <div key={ formattedHealthDate } className={ showing ? 'selected-date' : 'unselected-date'}>{ thisCardDate.toLocaleString('default', { day: 'numeric' }) }</div>
+        )
     }
+    dateDivs.push(<div><FontAwesomeIcon icon={ faChevronRight }/></div>)
 
     return (
         <>
-            <section className='date-nav-section'>
-                <div className='date-nav-container'>
-                    <div className='month-nav'>
-                        February
-                    </div>
-                    <div className=''>
-                        { startOfWeek().toString() }
-                        { user ? user.userName : 'no' } user
-                    </div>
+            <Container className='date-nav-section'>
+                <div className='month-nav'>
+                    { showDateObj.toLocaleString('default', { month: 'long'}) }
                 </div>
-            </section>
+                <div className='date-nav'>
+                    { dateDivs }
+                </div>
+            </Container>
         </>
     )
 }
